74f5c30a5fb98b74b364f82f820932752350c17d
322781188 <galnattibr@gmail.com>
2025-12-27 19:56:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,23 +4,26 @@ import yaml
 import os
 
 def load_model_and_predict():
-with open("config.yaml", "r") as f:
-    config = yaml.safe_load(f)
-
-model_file = config["model_name"]
-selected_features = config["features"]
-
-model = joblib.load(model_file)
-scaler = joblib.load("scaler.joblib")
-
-df = pd.read_csv("parkinsons.csv")
-
-X = df[selected_features]
-X_scaled = scaler.transform(X)
-y_true = df["status"]
-predictions = model.predict(X_scaled)
-
-from sklearn.metrics import accuracy_score
-accuracy = accuracy_score(y_true, predictions)
-
-return predictions
+    with open("config.yaml", "r") as f:
+        config = yaml.safe_load(f)
+    
+    model_file = config["path"]
+    selected_features = config["selected_features"]
+    
+    model = joblib.load(model_file)
+    scaler = joblib.load("scaler.joblib")
+    
+    df = pd.read_csv("parkinsons.csv")
+    
+    X = df[selected_features]
+    X_scaled = scaler.transform(X)
+    y_true = df["status"]
+    predictions = model.predict(X_scaled)
+    
+    from sklearn.metrics import accuracy_score
+    accuracy = accuracy_score(y_true, predictions)
+    
+    return predictions
+    
+if __name__ == "__main__":
+    load_model_and_predict()
